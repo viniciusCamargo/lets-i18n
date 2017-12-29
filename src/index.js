8319d8f6e3b4e0f2dba1561ba57aca09f3ab4e26
Vinicius Camargo <cmrg.vinicius@gmail.com>
2017-12-29 06:30:20 -0200

    prettier

--- src/index.js
+++ src/index.js
@@ -1,50 +1,58 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import get from 'lodash.get'
-import has from 'lodash.has'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import get from "lodash.get";
 
 export class TranslationProvider extends Component {
   // set the context name and type
   static childContextTypes = {
     translations: PropTypes.object,
     language: PropTypes.string
-  }
+  };
 
-  getChildContext () {
+  getChildContext() {
     // set the name and value of the props the children will receive
     return {
       translations: this.props.translations,
       language: this.props.language
-    }
+    };
   }
 
-  render () {
+  render() {
     // return all/any children, each with a "translations" and "language" prop
-    return this.props.children
+    return this.props.children;
   }
 }
 
-export const withTranslations = (ToWrap) => {
+const errors = {
+  notFound: translation =>
+    process.env.NODE_ENV === "production"
+      ? null
+      : `TRANSLATION_ERROR: "${translation}" does not exist.`,
+
+  noFiles: () =>
+    console.error("TRANSLATION_ERROR: Please, provide the files to translate.")
+};
+
+export const withTranslations = ToWrap => {
   class Wrapped extends Component {
     // set the the name and type of the context it will receive from parent
     static contextTypes = {
       translations: PropTypes.object,
       language: PropTypes.string
-    }
+    };
 
-    translate = (translationProp) => {
-      if (has(this.context.translations, translationProp)) {
-        return get(this.context.translations, translationProp)
-      } else {
-        console.error(`Translation Error: "${translationProp}" does not exist.`)
-      }
-    }
+    translate = translationProp =>
+      get(
+        this.context.translations,
+        translationProp,
+        errors.notFound(translationProp)
+      );
 
-    render () {
+    render() {
       // the received component with its "t" and "language" prop
-      return <ToWrap t={this.translate} language={this.context.language} />
+      return <ToWrap t={this.translate} language={this.context.language} />;
     }
   }
 
-  return Wrapped
-}
+  return Wrapped;
+};
