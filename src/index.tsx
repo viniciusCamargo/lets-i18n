580bb3f61ff3400e456b7e48d963a5d71d6f4e91
Vinicius Camargo <cmrg.vinicius@gmail.com>
2019-03-26 00:49:20 +0100

    Clean main and apply new prettier rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,57 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import get from 'lodash.get'
+import React, { Component } from "react";
+import get from "lodash.get";
 
-const devOrProd = (other, prod = null) =>
-  process.env.NODE_ENV !== 'production' ? other : prod
+function devOrProd(other, prod = null) {
+  return process.env.NODE_ENV !== "production" ? other : prod;
+}
 
-export class TranslationProvider extends Component {
-  // set the context name and type
-  static childContextTypes = {
-    translations: PropTypes.object,
-    language: PropTypes.string
-  }
+interface ITranslation {
+  translations: any;
+  language: string;
+}
 
+export class TranslationProvider extends Component<ITranslation> {
   getChildContext() {
     // set the name and value of the props the children will receive
     return {
       translations: this.props.translations,
       language: this.props.language
-    }
+    };
   }
 
   render() {
     // return all/any children, each with a "translations" and "language" prop
-    return this.props.children
+    return this.props.children;
   }
 }
 
-const returnLastKey = (str) => str.split('.').pop()
+function returnLastKey(str) {
+  return str.split(".").pop();
+}
 
 const errors = {
   notFound(translation) {
     return devOrProd(
       `TRANSLATION_ERROR: "${translation}" does not exist.`,
       returnLastKey(translation)
-    )
+    );
   },
   noFiles() {
     return console.error(
-      'TRANSLATION_ERROR: Please, provide the files to translate.'
-    )
+      "TRANSLATION_ERROR: Please, provide the files to translate."
+    );
   }
-}
+};
 
-export const withTranslations = (ToWrap) => {
-  class Wrapped extends Component {
-    // set the the name and type of the context it will receive from parent
-    static contextTypes = {
-      translations: PropTypes.object,
-      language: PropTypes.string
-    }
-
-    translate = (translationProp) =>
-      get(
+export function withTranslations(ToWrap) {
+  return class extends Component<ITranslation> {
+    translate(translationProp) {
+      return get(
         this.context.translations,
         translationProp,
         errors.notFound(translationProp)
-      )
+      );
+    }
 
     render() {
       // the received component with its "t" and "language" prop
@@ -71,9 +67,7 @@ export const withTranslations = (ToWrap) => {
           language={this.context.language}
           {...this.props}
         />
-      )
+      );
     }
-  }
-
-  return Wrapped
+  };
 }
